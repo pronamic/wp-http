7122b09c53b7e99a3e0bc8d03069276babc6def0
Reüel van der Steege <reuel@pronamic.nl>
2022-12-28 22:20:54 +0100

    Update @package in file comment.

--- src/Response.php
+++ src/Response.php
@@ -5,7 +5,7 @@
  * @author    Pronamic <info@pronamic.eu>
  * @copyright 2005-2022 Pronamic
  * @license   GPL-3.0-or-later
- * @package   Pronamic\WordPress\Pay\Payments
+ * @package   Pronamic\WordPress\Http
  */
 
 namespace Pronamic\WordPress\Http;
